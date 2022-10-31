cecec15f710c02f6223215f80f3ef9054781fe46
veryheavypickle <xavier.carroll01@gmail.com>
2022-10-31 21:06:56 +0100

    Added more -y flags

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ installDocker() {
 	# according to https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04
 	sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 	apt-cache policy docker-ce
-	sudo apt install docker-ce
+	sudo apt install docker-ce -y
 	# make sudoless execution of docker stuff
 	sudo usermod -aG docker ${USER}
 	su - ${USER}
