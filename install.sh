0f1d7b9653439174c70533b48d728b46a62fe06e
veryheavypickle <xavier.carroll01@gmail.com>
2022-10-31 21:04:05 +0100

    added -y

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ main() {
 installDocker() {
 	# install docker
 	# according to https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04
-	sudo apt install apt-transport-https ca-certificates curl software-properties-common
+	sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 	apt-cache policy docker-ce
